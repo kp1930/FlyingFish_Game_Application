644a108c42057aaccbe1206f1c29c60723d2f461
Kush Pandya <kushpandya111@gmail.com>
2019-08-19 18:40:54 +0530

    Created splash screen... Created Fish and make it fly... Created dots of color yellow, green, red as well add score...

--- app/src/main/java/theblackdiamonds/com/activities/MainActivity.java
+++ app/src/main/java/theblackdiamonds/com/activities/MainActivity.java
@@ -1,16 +1,39 @@
 package theblackdiamonds.com.activities;
 
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import theblackdiamonds.com.R;
+import theblackdiamonds.com.views.FlyingFishView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static long interval = 30;
+    private FlyingFishView gameView;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        gameView = new FlyingFishView(MainActivity.this);
+        setContentView(gameView);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+            }
+        }, 0, interval);
     }
 }
--- app/src/main/java/theblackdiamonds/com/views/FlyingFishView.java
+++ app/src/main/java/theblackdiamonds/com/views/FlyingFishView.java
@@ -1,8 +1,150 @@
 package theblackdiamonds.com.views;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.view.MotionEvent;
+import android.view.View;
+
+import theblackdiamonds.com.R;
+
 /**
  * Created by Kush Pandya on 8/19/2019.
  */
-public class FlyingFishView {
-    
+public class FlyingFishView extends View {
+
+    int score, canvasWidth, canvasHeight;
+    int yellowX, yellowY, yellowSpeed = 16;
+    int greenX, greenY, greenSpeed = 20;
+    int redX, redY, redSpeed = 25;
+    private Bitmap[] fish = new Bitmap[2];
+    private Bitmap backgroundImage;
+    private Bitmap[] life = new Bitmap[2];
+    private Paint scorerPaint = new Paint();
+    private int fishX = 10, fishY, fishSpeed;
+    private Paint yellowPaint = new Paint();
+    private Paint greenPaint = new Paint();
+    private Paint redPaint = new Paint();
+
+    private boolean isTouch = false;
+
+    public FlyingFishView(Context context) {
+        super(context);
+        fish[0] = BitmapFactory.decodeResource(getResources(), R.drawable.fish1);
+        fish[1] = BitmapFactory.decodeResource(getResources(), R.drawable.fish2);
+
+        backgroundImage = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+
+        yellowPaint.setColor(Color.YELLOW);
+        yellowPaint.setAntiAlias(false);
+
+        greenPaint.setColor(Color.GREEN);
+        greenPaint.setAntiAlias(false);
+
+        redPaint.setColor(Color.RED);
+        redPaint.setAntiAlias(false);
+
+        scorerPaint.setColor(Color.WHITE);
+        scorerPaint.setTextSize(70);
+        scorerPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        scorerPaint.setAntiAlias(true);
+
+        life[0] = BitmapFactory.decodeResource(getResources(), R.drawable.hearts);
+        life[1] = BitmapFactory.decodeResource(getResources(), R.drawable.heart_grey);
+
+        fishY = 550;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvasHeight = getHeight();
+        canvasWidth = getWidth();
+
+        canvas.drawBitmap(backgroundImage, 0, 0, null);
+
+        int minFishY = fish[0].getHeight();
+        int maxFishY = canvasHeight - minFishY * 3;
+        fishY = fishY + fishSpeed;
+
+        if (fishY < minFishY) {
+            fishY = minFishY;
+        }
+        if (fishY > maxFishY) {
+            fishY = maxFishY;
+        }
+
+        fishSpeed = fishSpeed + 2;
+
+        if (isTouch) {
+            canvas.drawBitmap(fish[1], fishX, fishY, null);
+            isTouch = false;
+        } else {
+            canvas.drawBitmap(fish[0], fishX, fishY, null);
+        }
+
+        yellowX = yellowX - yellowSpeed;
+
+        if (isHit(yellowX, yellowY)) {
+            score = score + 10;
+            yellowX = -100;
+        }
+
+        if (yellowX < 0) {
+            yellowX = canvasWidth + 21;
+            yellowY = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
+        }
+        canvas.drawCircle(yellowX, yellowY, 25, yellowPaint);
+
+        greenX = greenX - greenSpeed;
+
+        if (isHit(greenX, greenY)) {
+            score = score + 20;
+            greenX = -100;
+        }
+
+        if (greenX < 0) {
+            greenX = canvasWidth + 21;
+            greenY = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
+        }
+        canvas.drawCircle(greenX, greenY, 25, greenPaint);
+
+        redX = redX - redSpeed;
+
+        if (isHit(redX, redY)) {
+            redX = -100;
+        }
+
+        if (redX < 0) {
+            redX = canvasWidth + 21;
+            redY = (int) Math.floor(Math.random() * (maxFishY - minFishY)) + minFishY;
+        }
+        canvas.drawCircle(redX, redY, 30, redPaint);
+
+        canvas.drawText("Score : " + score, 20, 60, scorerPaint);
+
+        canvas.drawBitmap(life[0], 580, 10, null);
+        canvas.drawBitmap(life[0], 680, 10, null);
+        canvas.drawBitmap(life[0], 780, 10, null);
+    }
+
+    public boolean isHit(int x, int y) {
+        return fishX < x && x < (fishX + fish[0].getWidth()) && fishY < y && y < (fishY + fish[0].getHeight());
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            isTouch = true;
+            fishSpeed = -22;
+        }
+        return true;
+    }
 }
